45bee808975cd127c8979db6216af06c6d04a42c
laurentiu <laurentiucosma25@gmail.com>
2013-11-26 22:45:41 +0200

    adaugat cod pentru a lua data sistemului, adaugat o clasa pentru timp dar sa vedem care este eficienta

---
+++ plannetWPF/DateClass.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+
+namespace plannetWPF
+{
+    public class DateClass
+    {
+        private int day;
+        private string month;
+        private int year;
+
+        private DateTime _date;
+
+        public DateClass(DateTime time)
+        {
+            GetDate = time;
+            
+            //sa vad daca mai trebuie sa mai lucrez la asta sau o sa las asa
+            SetMonth();
+        }
+
+        private void SetMonth()
+        {
+            month = GetDate.ToString( "MMMM" , new CultureInfo("ro-RO"));
+            Console.WriteLine(month);
+
+        }
+
+
+        public DateTime GetDate
+        {
+            get
+            {
+                return _date;
+            }
+            set
+            {
+                //nu stiu daca sa mai adaug niste formatarii speciale
+                _date = value;
+            }
+
+        }
+
+        public string GetMonth
+        {
+            get
+            {
+                return month;
+            }
+
+        }
+
+    }
+}
---
+++ plannetWPF/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Drawing;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+
+namespace plannetWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        NotifyIcon notifyIcon;
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DateClass date = new DateClass(DateTime.Now);
+            
+            //nu stiu sigur ce inseamna asta
+            day.Content = date.GetDate.Day;
+            month.Content = date.GetMonth;
+            year.Content = date.GetDate.Year;
+
+            month.Measure(new System.Windows.Size(double.PositiveInfinity, double.PositiveInfinity));
+
+            Console.WriteLine(month.Width + " " + DateCollection.Width);
+            
+            
+        }
+
+        private void closeLBL_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Close();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+    }
+}
