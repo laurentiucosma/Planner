4ca56f421b492dd71b0d4d78bea0d66f50bb5faf
laurentiu <laurentiucosma25@gmail.com>
2013-12-01 13:06:39 +0200

    reusit sa aranjez textul in grid pana la urma

--- plannetWPF/MainWindow.xaml.cs
+++ plannetWPF/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Forms;
+using System.Globalization;
 
 namespace plannetWPF
 {
@@ -31,13 +32,11 @@ namespace plannetWPF
             DateClass date = new DateClass(DateTime.Now);
             
             //nu stiu sigur ce inseamna asta
-            day.Content = date.GetDate.Day;
-            month.Content = date.GetMonth;
+            day.Content = date.GetDate.Day > 9 ? date.GetDate.Day.ToString() : "0" + date.GetDate.Day ;
+            month.Content = UpperCaseFirstLetter( date.GetMonth);
             year.Content = date.GetDate.Year;
 
-            month.Measure(new System.Windows.Size(double.PositiveInfinity, double.PositiveInfinity));
-
-            Console.WriteLine(month.Width + " " + DateCollection.Width);
+           
             
             
         }
@@ -72,6 +71,50 @@ namespace plannetWPF
 
         }
 
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+            //pentru masurarea textului
+            FormattedText dayFText = GetTextSize(day.Content.ToString(), "Candara", 60);
+            FormattedText monthFText = GetTextSize(month.Content.ToString(), "Segoe", 12);
+            FormattedText yearFText = GetTextSize(year.Content.ToString(), "Candara", 22);
+                                                   
+
+            Console.WriteLine(dayFText.Width + " " + monthFText.Width + " " + yearFText.Width + " " + dateCollection.Width);
+            day.Margin = new Thickness((dateCollection.Width - dayFText.Width) / 2 - 10,
+                                        day.Margin.Top,
+                                        (dateCollection.Width - dayFText.Width) / 2,
+                                        0);
+            month.Margin = new Thickness((dateCollection.Width - monthFText.Width)/2 -10,
+                                          month.Margin.Top,
+                                          (dateCollection.Width - monthFText.Width) / 2,
+                                          month.Margin.Bottom);
+            year.Margin = new Thickness((dateCollection.Width - yearFText.Width) / 2 - 10,
+                                          year.Margin.Top,
+                                          (dateCollection.Width - yearFText.Width) / 2,
+                                          year.Margin.Bottom);
+
+
+
+
+        }
+
+        private FormattedText GetTextSize(string textToReturnFormat, string fontName, int fontSize)
+        {
+            FormattedText returnedFText = new FormattedText(textToReturnFormat,
+                                                            new CultureInfo("ro-RO"),
+                                                            System.Windows.FlowDirection.LeftToRight,
+                                                            new Typeface(fontName),
+                                                            fontSize,
+                                                            System.Windows.Media.Brushes.Black);
+            return returnedFText;
+        }
+
+        private string UpperCaseFirstLetter(string input)
+        {
+            return input[0].ToString().ToUpper() + input.Substring(1);
+           
+        }
         
         
 
